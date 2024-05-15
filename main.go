16edf881d1e8657d3942cf0f4dd4ba3e0c8e46c1
Andrew Towers <andrew.towers@outlook.com>
2024-05-15 06:51:15 +0100

    Registration

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
@@ -23,21 +24,26 @@ func main() {
 		c.HTML(http.StatusOK, "register.tmpl", gin.H{})
 	})
 	router.POST("register", func(c *gin.Context) {
+		c.Request.ParseForm()
 		email := c.Request.Form.Get("email")
-		password := c.Request.Form.Get("password")
+		password := []byte(c.Request.Form.Get("password"))
 
-		if email == "" || password == "" {
+		if email == "" || len(password) < 4 || len(password) > 72 {
 			c.String(http.StatusBadRequest, "Please enter email and password!")
 			return
 		}
-
+		bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		c.JSON(http.StatusOK, gin.H{"email": email, "password": password})
 	})
 
 	threadHandlers(router)
 	categoryHandler(router)
 
+	router.StaticFile("/htmx.min.js.js", "./assets/htmx.min.js.js")
+
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{"categories": &cat})
 	})
+
 	app.Run()
 }
